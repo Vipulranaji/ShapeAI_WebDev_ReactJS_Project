79ca8c81afecc6ec098c7d381587c64f96cbd220
Vipulranaji <85949487+Vipulranaji@users.noreply.github.com>
2021-06-27 22:31:11 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1> Introduction to React</h1>
+      <ul>
+        <li>What is React?</li>
+        <li>Who Developed React?</li>
+        <li>Basics of React JS.</li>
+        <li>Alternatives to React.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Info;
